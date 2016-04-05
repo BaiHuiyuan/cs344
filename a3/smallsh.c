c60ece697ebf641130a7d31f846a5974a2f698da
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-04-05 07:08:24 -0700

    Begin implementing cd builtin

--- a3/smallsh.c
+++ a3/smallsh.c
@@ -26,16 +26,33 @@
 
 /* Forward declarations */
 void command_prompt();
-void 
-exit_shell() {
+static void sigHandler(int sig);
+
+
+void change_directory(char * dir) {
+	// If empty string passed in, go to the HOME path
+	if (strcmp(dir, "") == 0) {
+		dir = "HOME path directory";
+	}
+	printf("Changing directory to:  %s\n", dir);
+
+	chdir(dir);
+
+}
+
+void exit_shell() {
 	printf("Exiting...\n");
 	// Kill all child processes / jobs
 	// terminate smallsh itself by calling exit with success signal (0)
 	exit(0);
 }
 
-void 
-command_prompt() {
+void command_prompt() {
+	// Start up signal handler
+	if (signal(SIGINT, sigHandler) == SIG_ERR) {
+		printf("SIG_ERR received\n");
+	}
+
 	// support MAX_CHAR characters + 1 for null byte
 	char input[MAX_CHAR+1]; 
 	int repeat = 1;
@@ -47,15 +64,29 @@ command_prompt() {
 		input[strlen(input)-1] = '\0';
 
 		// DEBUG: Echo the input
-		printf("You input: %s\n", input);
+		printf("input: %s\n", input);
 
 		/* Parse the input  */
 		
 		int arg_count;
 
-
+		// Check for the built-in commands:
 		if (strcmp(input, "exit") == 0) {
 			exit_shell();
+		}
+		else if (strcmp(input, "cd") == 0) {
+			change_directory("");
+		}
+
+
+		// Ignore lines that start with # as comments
+		else if (input[0] == '#') {
+			continue;
+		}
+
+		// Else if only whitespace or a newline entered then just reprompt
+		else {
+			continue;
 		}
 	}
 
@@ -65,20 +96,18 @@ command_prompt() {
 // but instead terminates the foreground process
 // Cite: tLPI Page 399
 
-static void
-sigHandler(int sig) {
-	printf("sigHandler called\n");
+static void sigHandler(int sig) {
+	if (sig == SIGINT) {
+		printf("SIGINT caught\n");
+		// Figure out how to terminate just the foreground child process and do that here
+	}
 	return;
 }
 
 
-int 
-main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-	// Start up signal handler
-	// if (signal(SIGINT, sigHandler) == SIG_ERR) {
-	// 	errExit("signal");
-	// }
+
 	command_prompt();
 	return 0;
 }
