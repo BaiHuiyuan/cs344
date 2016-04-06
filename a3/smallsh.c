71155e46e7bf0171e97a2838b6354dad0eca9258
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-04-05 17:18:36 -0700

    Add much of parsing required

--- a3/smallsh.c
+++ a3/smallsh.c
@@ -23,10 +23,10 @@
 #define MAX_CHAR 2048
 #define MAX_ARGS 512
 
-
 /* Forward declarations */
 void command_prompt();
 static void sigHandler(int sig);
+void print_array (char ** arguments, int size);
 
 /* Externs */
 extern char **environ; // pointer to strings listing the environment variables
@@ -71,59 +71,95 @@ void command_prompt() {
 		// Read: Prompt user, get input, and null terminate their string
 		printf(": ");
 		fgets(input, sizeof(input), stdin);
-		input[strlen(input)-1] = '\0';
+		if (strlen(input) > 0)
+			input[strlen(input)-1] = '\0'; // removes the newline and replaces it with null
 
 		// DEBUG: Echo the input
 		printf("input: %s\n", input);
 
-		/* Parse the input  */	
+		// Variables used in parsing input	
 		int arg_count = 0;
 		char * arguments[MAX_ARGS];
 		char * command;
+		char * input_file;
+		char * output_file;
 
 		// Ignore lines that start with # as comments
 		if (input[0] == '#') {
 			continue;
 		}
+		
+		// Check that the input was not null before testing further
+		if (command = strtok(input, " ")) {
+			// Tokenize the line, storing words until all arguments are read
+			while(arguments[arg_count] = strtok(NULL, " ")) {
+				// Add logic here to parse for arguments -- check if arguments[arg_count][0] == '-'
+				char * word = arguments[arg_count];
+				
+				// If the current 'word' is begins with '#', change it to null and stop reading additional arguments
+				if (word[0] == '#') {
+					word[0] = '\0';
+					break;
+				}
 
-		command = strtok(input, " ");
-		while(arguments[arg_count] = strtok(NULL, " ")) {
-			printf("arguments[%d]: %s\n", arg_count, arguments[arg_count]);
-			// Add logic here to parse for arguments -- check if arguments[arg_count][0] == '-'
-			arg_count++;
+				// If the input redirection operator is present, set input_redirect to 1 (true) and
+				// grab the filename (which should be the next word)
+				if (strcmp(word, "<") == 0) {
+					printf("Input redirection operator found\n");
+					// 
+				}
+				else if (strcmp(word, ">") == 0) {
+					printf("Output redirection operator found\n");
+				}
+				
+				arg_count++;
+			}
+
+			printf("arg_count: %d\n", arg_count);
+
+			// Set the background mode to true if the last 'argument' is '&'
+			if (arg_count > 1) {
+				if (strcmp(arguments[arg_count - 1], "&") == 0) {
+					printf("Background process.\n");
+				}
+			}
+
+
+			printf("command = %s\n", command);
+			print_array(arguments, arg_count);
+
+			// Check for the built-in commands:
+			if (strcmp(command, "exit") == 0) {
+				exit_shell();
+			}
+
+			// If no-arguments 'cd' command
+			else if (strcmp(command, "cd") == 0) {
+				if (arg_count == 0)
+					change_directory("");
+				else
+					change_directory(arguments[0]);
+			}
+
+			// If cd with a directory passed
+			// else if (str)
+
+
+
+			// Else if only whitespace or a newline entered then just reprompt
+			else {
+				continue;
+			}
+		}	
 		}
-		printf("command = %s\n", command);
 
-		// Check for the built-in commands:
-		if (strcmp(command, "exit") == 0) {
-			exit_shell();
-		}
-
-		// If no-arguments 'cd' command
-		else if (strcmp(command, "cd") == 0) {
-			if (arg_count == 0)
-				change_directory("");
-			else
-				change_directory(arguments[0]);
-		}
-
-		// If cd with a directory passed
-		// else if (str)
-
-
-
-		// Else if only whitespace or a newline entered then just reprompt
-		else {
-			continue;
-		}
-	}
+		
 
 }
 
-// Need to build a signal handler for SIGINT that does NOT terminate this process
+// signal handler for SIGINT that does NOT terminate this shell,
 // but instead terminates the foreground process
-// Cite: tLPI Page 399
-
+// Cite: TLPI Page 399
 static void sigHandler(int sig) {
 	if (sig == SIGINT) {
 		printf("SIGINT caught\n");
@@ -133,6 +169,16 @@ static void sigHandler(int sig) {
 }
 
 
+// Used for debugging purposes
+void print_array (char ** arguments, int n) {
+	int i = 0;
+	while (i < n) {
+		printf("arg%d: %s\n", i, arguments[i++]);
+	}
+}
+
+
+// main
 int main(int argc, char const *argv[])
 {
 
